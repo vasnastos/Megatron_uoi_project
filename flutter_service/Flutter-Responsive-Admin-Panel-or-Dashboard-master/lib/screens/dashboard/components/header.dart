64cd907956d95a8a9ca35ddc6c212222d72a28c4
johnrod <68949961+johnrodusakis@users.noreply.github.com>
2023-02-15 14:42:03 +0200

    Implemented new flutter project with template (with errors)

--- flutter_service/Flutter-Responsive-Admin-Panel-or-Dashboard-master/lib/screens/dashboard/components/header.dart
+++ flutter_service/Flutter-Responsive-Admin-Panel-or-Dashboard-master/lib/screens/dashboard/components/header.dart
@@ -18,7 +18,7 @@ class Header extends StatelessWidget {
         if (!Responsive.isDesktop(context))
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: context.read<MenuController>().controlMenu,
+            onPressed: context.read<MenuHandler>().controlMenu,
           ),
         if (!Responsive.isMobile(context))
           Text(
